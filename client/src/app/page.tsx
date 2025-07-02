22bc7fdcaead9af66b61f7dba2911898dbefb47d
Dev-639 <devissrani@gmail.com>
2025-07-02 14:17:45 +0530

    Navbar setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-4xl font-bold m-16 text-red-950">
+      hi
+    </div>
+  );
+}
